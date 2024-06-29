084ef1ea341a951a7b1ac8ad122e341201bbc829
Thanat Wongsamut <thanat.wongsamut@agoda.com>
2024-06-29 13:11:27 +0700

    [UPDATE] testcase

--- tests/viteBuildStatsPlugin.spec.ts
+++ tests/viteBuildStatsPlugin.spec.ts
@@ -1,22 +1,36 @@
 import type { CommonMetadata, ViteBuildData } from '../src/types';
 import { viteBuildStatsPlugin } from '../src/viteBuildStatsPlugin';
 import { getCommonMetadata, sendBuildData } from '../src/common';
+import { BigIntStats, PathLike, Stats, promises as fs } from 'fs';
+import path from 'path';
 
 jest.mock('../src/common', () => ({
   getCommonMetadata: jest.fn(),
   sendBuildData: jest.fn(),
 }));
 
+jest.mock('fs', () => ({
+  promises: {
+    stat: jest.fn(),
+  },
+}));
+
 const mockedGetCommonMetadata = getCommonMetadata as jest.MockedFunction<
   typeof getCommonMetadata
 >;
 const mockedSendBuildData = sendBuildData as jest.MockedFunction<typeof sendBuildData>;
+const mockedFsStat = fs.stat as jest.MockedFunction<typeof fs.stat>;
 
 describe('viteBuildStatsPlugin', () => {
   const expected: ViteBuildData = {
     type: 'vite',
     viteVersion: '1.2.3',
-  } as ViteBuildData;
+    bundleFiles: {
+      'file1.js': 1000,
+      'file2.js': 2000,
+    },
+    bundleSize: 3000,
+  } as unknown as ViteBuildData;
 
   beforeEach(() => {
     jest.resetAllMocks();
@@ -32,9 +46,21 @@ describe('viteBuildStatsPlugin', () => {
     mockedGetCommonMetadata.mockReturnValue({} as CommonMetadata);
     mockedSendBuildData.mockReturnValue(Promise.resolve());
 
+    // mock fs.stat
+    mockedFsStat.mockImplementation((path: PathLike) => {
+      if (path.toString().endsWith('file1.js')) {
+        return Promise.resolve({ size: 1000 } as Stats);
+      } else if (path.toString().endsWith('file2.js')) {
+        return Promise.resolve({ size: 2000 } as Stats);
+      } else {
+        return Promise.reject(new Error('File not found'));
+      }
+    });
+
     const plugin = viteBuildStatsPlugin('my custom identifier');
     (plugin.buildStart as () => void).bind({ meta: { rollupVersion: '1.2.3' } })();
     (plugin.buildEnd as () => void)();
+    await (plugin.writeBundle as any)({ dir: 'dist' }, { 'file1.js': {}, 'file2.js': {} });
     await (plugin.closeBundle as () => Promise<void>)();
 
     expect(mockedGetCommonMetadata).toBeCalledWith(100, 'my custom identifier');
@@ -58,9 +84,21 @@ describe('viteBuildStatsPlugin', () => {
       },
     } as unknown as typeof process;
 
+    // mock fs.stat
+    mockedFsStat.mockImplementation((path: PathLike) => {
+      if (path.toString().endsWith('file1.js')) {
+        return Promise.resolve({ size: 1000 } as Stats);
+      } else if (path.toString().endsWith('file2.js')) {
+        return Promise.resolve({ size: 2000 } as Stats);
+      } else {
+        return Promise.reject(new Error('File not found'));
+      }
+    });
+
     const plugin = viteBuildStatsPlugin();
     (plugin.buildStart as () => void).bind({ meta: { rollupVersion: '1.2.3' } })();
     (plugin.buildEnd as () => void)();
+    await (plugin.writeBundle as any)({ dir: 'dist' }, { 'file1.js': {}, 'file2.js': {} });
     await (plugin.closeBundle as () => Promise<void>)();
 
     expect(mockedGetCommonMetadata).toBeCalledWith(100, 'default_value');
