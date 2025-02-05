4ed2f5558f5aa7041ac6b051c6af277eb488c900
Ratchapol Anantawat <ratchapol.anantawat@agoda.com>
2025-02-05 10:49:23 +0700

    config eslint to ignore files in .gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import { includeIgnoreFile } from '@eslint/compat';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,8 +15,10 @@ const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
+  includeIgnoreFile(gitignorePath),
   ...compat.extends(
     'plugin:@typescript-eslint/recommended',
     'prettier',
