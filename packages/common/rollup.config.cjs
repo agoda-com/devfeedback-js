74cdfc55d9373aad3de65b783fa2b429e80885dc
Ratchapol Anantawat <ratchapol.anantawat@agoda.com>
2025-02-19 16:32:07 +0700

    chore: add roll up to publish cjs

---
+++ packages/common/rollup.config.cjs
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { withNx } = require('@nx/rollup/with-nx');
+
+module.exports = withNx(
+  {
+    main: './src/index.ts',
+    outputPath: './dist',
+    tsConfig: './tsconfig.lib.json',
+    compiler: 'tsc',
+    format: ['esm', 'cjs'],
+  },
+  {
+    // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
+    // e.g.
+    // output: { sourcemap: true },
+  },
+);
